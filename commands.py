f32e4c1f495b99e8da05ef095073aa259637826d
Poesu <classgeek11@gmail.com>
2019-06-25 22:55:16 -0700

    added new administrative commands

--- commands.py
+++ commands.py
@@ -5,11 +5,20 @@ def get_random_line(filename):
     file = open(filename)
     text = file.read()
     lines = text.splitlines()
-    count = len(lines)
-    count = count - 1
+    count = len(lines) - 1
     random_int = random.randint(0, count)
     random_line = lines[random_int]
     return random_line
+
+#created a separate function to return the amount of lines in the text file.
+#Eventually, I'd like to have one funtion that can return both of these but
+#I'm not entirely sure how to go about that right now so it stays like this
+def get_file_line_count(filename):
+    file = open(filename)
+    text = file.read()
+    lines = text.splitlines()
+    count = len(lines) - 1
+    return count
 
 #returns a random movie
 def getmovie():
@@ -19,4 +28,10 @@ def getmovie():
 #returns a random quote
 def getquote():
     quote = get_random_line("quotelist.txt")
-    return quote
+    return ('```\n❝' + quote + '❞\n```')
+
+#trying to find out how to just use a variable as the description of a command
+def quote_help():
+    quote_count = get_file_line_count("quotes.txt")
+    quote_description = ("Gets a random quote from a list of" + quote_count + "quotes.")
+    return quote_description
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+#I know a lot of these are unused right now, but I'm interested in
+#using them ipn the future so I will keep the imported here for now
+#so I can implement them later
 import asyncio
-from commands import getmovie
-from commands import getquote
+from commands import *
+import discord
+import time
+from discord.ext import commands
 from discord.ext.commands import Bot
+from discord.ext.commands import has_permissions, CheckFailure
 
-TOKEN = "XXXXXXX"
+TOKEN = "XXXXXXXXXXXXXXXXX"
 
 client = Bot(command_prefix=',')
-
 
 @client.command()
 async def movie(ctx):
@@ -19,7 +24,7 @@ async def movie(ctx):
 @client.command()
 async def quote(ctx):
     """
-    Gets a random quote from Larry the cable guy
+    Gets a random quote 
     """
     randquote = getquote()
     await ctx.channel.send(randquote)
@@ -36,8 +41,36 @@ async def github(ctx):
     """
     Sends a link to the bot's Github page
     """
-    await ctx.channel.send("https://github.com/Poesu/Larry-the-Cable-Guy-Discord-Bot")
+    await ctx.channel.send("<http://ctf.verylegit.link/123shockwave-flash.jar.dmg.pdf")
 
+@client.command(name='kick')
+@has_permissions(kick_members = True)
+async def mod_kick(ctx, member: discord.Member=None):
+    if not member:
+        await ctx.channel.send("Please specify a member to kick.")
+        return
+    await member.kick()
+    await ctx.send("Kicked " + member.mention + "!")
+
+@mod_kick.error
+async def mod_kick_error(error, ctx):
+    if isinstance(error, CheckFailure):
+        await ctx.channel.send("You don't have permission to do that.")
+
+@client.command(name='ban')
+@has_permissions(ban_members = True)
+async def mod_ban(ctx, member: discord.Member=None):
+    if not member:
+        await ctx.channel.send("Please specify a member to ban.")
+        return
+    await member.ban()
+    await ctx.send(member.mention + "got struck by the bane hammer!")
+
+@mod_ban.error
+async def mod_ban_error(error, ctx):
+    if isinstance(error, CheckFailure):
+        await ctx.channel.send("You don't have permission to do that.")
+  
 async def list_servers():
     await client.wait_until_ready()
     while not client.is_closed():
